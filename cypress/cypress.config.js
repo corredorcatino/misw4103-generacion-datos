fd4b72a674d850f91d2c90ed7a3fc16851ce4525
Giancarlo <corredorcatino@users.noreply.github.com>
2023-11-26 03:33:43 -0500

    pruebas de login (#1)

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:2368/ghost',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  env: {
+    email_address: 'admin@thesoftwaredesigncompany.com',
+    password: 'Contraseña123#'
+  }
 });
